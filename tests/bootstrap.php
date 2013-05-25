59d9187c52949e9404b5f2d05c1026fadb2f4538
Jan Unger <ju@juit.de>
2013-05-25 23:34:42 +0200

    Fiddling on Travis config

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,5 +2,5 @@
 
 error_reporting(E_ALL | E_STRICT);
 
-$loader = require_once __DIR__ . '/../vendor/autoload.php';
+$loader = require __DIR__ . '/../vendor/autoload.php';
 $loader->add('Doctrine\Tests', __DIR__ . '/../vendor/doctrine/dbal/tests/');
