cc902b404d6fd933fe37ff879714947b2fbd450f
Jan Unger <ju@juit.de>
2015-10-28 16:41:33 +0100

    Refactoring

--- tests/JUIT/Tests/Doctrine/DBAL/Types/UtcDateTimeTypeTest.php
+++ tests/JUIT/Tests/Doctrine/DBAL/Types/UtcDateTimeTypeTest.php
@@ -11,10 +11,10 @@ use JUIT\Doctrine\DBAL\Types\UtcDateTimeType;
 class UtcDateTimeTypeTest extends \PHPUnit_Framework_TestCase
 {
     /** @var MySqlPlatform */
-    protected $platform;
+    private $platform;
 
     /** @var UtcDateTimeType */
-    protected $type;
+    private $SUT;
 
     public static function setUpBeforeClass()
     {
@@ -24,7 +24,7 @@ class UtcDateTimeTypeTest extends \PHPUnit_Framework_TestCase
     protected function setUp()
     {
         $this->platform = new MySqlPlatform();
-        $this->type = Type::getType('utcdatetime');
+        $this->SUT = Type::getType('utcdatetime');
     }
 
     /** @test */
@@ -33,7 +33,7 @@ class UtcDateTimeTypeTest extends \PHPUnit_Framework_TestCase
         $date = new DateTime('1973-05-12 00:00:00', new \DateTimeZone('Europe/Berlin'));
 
         $expected = '1973-05-11 23:00:00';
-        $actual = $this->type->convertToDatabaseValue($date, $this->platform);
+        $actual = $this->SUT->convertToDatabaseValue($date, $this->platform);
 
         $this->assertEquals($expected, $actual);
     }
@@ -44,7 +44,7 @@ class UtcDateTimeTypeTest extends \PHPUnit_Framework_TestCase
         $date = new DateTime('2013-05-12 00:00:00', new \DateTimeZone('Europe/Berlin'));
 
         $expected = '2013-05-11 22:00:00';
-        $actual = $this->type->convertToDatabaseValue($date, $this->platform);
+        $actual = $this->SUT->convertToDatabaseValue($date, $this->platform);
 
         $this->assertEquals($expected, $actual);
     }
@@ -52,7 +52,7 @@ class UtcDateTimeTypeTest extends \PHPUnit_Framework_TestCase
     /** @test */
     public function it_converts_a_database_value_to_a_date_time_instance_with_default_date_time_zone()
     {
-        $actual = $this->type->convertToPHPValue('2013-05-11 22:00:00', $this->platform);
+        $actual = $this->SUT->convertToPHPValue('2013-05-11 22:00:00', $this->platform);
 
         $this->assertInstanceOf(DateTime::class, $actual);
         $this->assertEquals(date_default_timezone_get(), $actual->getTimezone()->getName());
@@ -66,26 +66,26 @@ class UtcDateTimeTypeTest extends \PHPUnit_Framework_TestCase
     /** @test */
     public function it_converts_database_null_to_php_null()
     {
-        $this->assertNull($this->type->convertToPHPValue(null, $this->platform));
+        $this->assertNull($this->SUT->convertToPHPValue(null, $this->platform));
     }
 
     /** @test */
     public function it_converts_php_null_to_database_null()
     {
-        $this->assertNull($this->type->convertToDatabaseValue(null, $this->platform));
+        $this->assertNull($this->SUT->convertToDatabaseValue(null, $this->platform));
     }
 
     /** @test */
     public function it_throws_an_exception_on_invalid_input_from_database()
     {
         $this->setExpectedException(ConversionException::class);
-        $this->type->convertToPHPValue('abcde', $this->platform);
+        $this->SUT->convertToPHPValue('abcde', $this->platform);
     }
 
     /** @test */
     public function it_throws_an_exception_on_invalid_input_from_php()
     {
         $this->setExpectedException(ConversionException::class);
-        $this->type->convertToDatabaseValue('abcde', $this->platform);
+        $this->SUT->convertToDatabaseValue('abcde', $this->platform);
     }
 }
